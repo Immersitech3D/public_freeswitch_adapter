c604c016611a2daa3a1ee2b153ac6426559e8354
imosebrook-immersitech <imosebrook@immersitech.net>
2021-07-20 10:09:44 -0400

    fix build errors
    now it will compile properly

--- mod_conference/imm_processor_wrapper.c
+++ mod_conference/imm_processor_wrapper.c
@@ -5,22 +5,24 @@
 #define IMM_PROCESSOR_OUTPUT_ID 1
 
 imm_handle create_immersitech_processor() {
+	imm_error_code error_code;
+	imm_library_configuration config;
+	imm_participant_configuration participant_config;
+	imm_handle handle
+	
 	// Let us log to freeswitch as it is easier to debug
 	imm_enable_logging(true);
 
 	// Create the library, a room, and our input and output participants
-	imm_error_code error_code;
-	imm_library_configuration config;
 	config.interleaved = true; // Audio in freeswitch is always interleaved
 	config.output_number_channels = IMM_NUMBER_CHANNELS;
 	config.output_number_frames = IMM_NUMBER_FRAMES;
 	config.output_sampling_rate = IMM_SAMPLING_RATE;
 	config.spatial_quality = IMM_SPATIAL_QUALITY;
-	imm_handle handle = imm_initialize_library(IMM_LICENSE_PATH, IMM_ROOM_LAYOUT_PATH, IMM_WEBSOCKET_PATH, config, &error_code);
+	handle = imm_initialize_library(IMM_LICENSE_PATH, IMM_ROOM_LAYOUT_PATH, IMM_WEBSOCKET_PATH, config, &error_code);
 	imm_create_room(handle, IMM_PROCESSOR_ROOM_ID);
 
 	// Currently assume all input audio is resampled and same number of channels as output
-	imm_participant_configuration participant_config;
 	participant_config.input_number_channels = IMM_NUMBER_CHANNELS;
 	participant_config.input_sampling_rate = IMM_SAMPLING_RATE;
 	participant_config.type = IMM_PARTICIPANT_SOURCE_ONLY;
@@ -43,7 +45,7 @@ void immersitech_process(imm_handle handle, short* input, int num_in_frames, sho
 }
 
 imm_error_code immersitech_set_state(imm_handle handle, imm_audio_control control, int value) {
-	imm_set_all_participants_state(handle, IMM_PROCESSOR_ROOM_ID, control, value);
+	return imm_set_all_participants_state(handle, IMM_PROCESSOR_ROOM_ID, control, value);
 }
 
 void print_library_info(imm_handle handle) {
@@ -51,7 +53,7 @@ void print_library_info(imm_handle handle) {
 	printf("Version: %s\n", imm_get_version());
 	printf("License: %s\n", imm_get_license_info(handle));
 	imm_library_configuration config = imm_get_library_configuration(handle);
-	printf("Interleaved: %b\n", config.interleaved);
+	printf("Interleaved: %d\n", config.interleaved);
 	printf("Number Channels: %i\n", config.output_number_channels);
 	printf("Number Frames: %i\n", config.output_number_frames);
 	printf("Sampling Rate: %i\n", config.output_sampling_rate);
